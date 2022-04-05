c50033182f9cc7b6731ced4b8e6af7a9033e0737
andrewdrive <email.com>
2022-04-05 17:15:19 +0300

    models-1.1

--- messproj/api/admin.py
+++ messproj/api/admin.py
@@ -1,3 +1,11 @@
+from turtle import circle
 from django.contrib import admin
+from api.models import Distribution, Client, Message
+
+
+admin.site.register(Distribution, admin.ModelAdmin)
+admin.site.register(Client, admin.ModelAdmin)
+admin.site.register(Message, admin.ModelAdmin)
+
 
 # Register your models here.
--- messproj/api/models.py
+++ messproj/api/models.py
@@ -8,7 +8,12 @@ TIMEZONES = tuple(zip(pytz.all_timezones, pytz.all_timezones))
 class Client(models.Model):
      phone_regex = RegexValidator(regex=r'^\d{11}', message="Phone number in the format: '7XXXXXXXXXX'")
      phone_number = models.CharField(validators=[phone_regex], max_length=12, blank=True, verbose_name='номер телефона клиента')
-     mobile_operator_code = models.CharField(max_length=3, default=phone_number[1:4], verbose_name='код мобильного оператора')
+
+     #mobile_operator_code = models.CharField(max_length=3, default=phone_number[1:4], verbose_name='код мобильного оператора')
+     @property
+     def mobile_operator_code(self):
+          return self.phone_number[1:4]
+
      tag = models.CharField(max_length=30, blank=True, verbose_name='тег(произвольная метка)')
      timezone = models.CharField(max_length=32, choices=TIMEZONES, default='UTC', verbose_name='часовой пояс')
 
