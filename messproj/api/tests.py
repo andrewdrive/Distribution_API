480db47b64b4bf417033187fbd08e4136f4eaf46
andrewdrive <email.com>
2022-04-19 17:38:46 +0300

    tests

--- messproj/api/tests.py
+++ messproj/api/tests.py
@@ -1,4 +1,15 @@
 from django.test import TestCase
+from .models import Distribution
+from django.utils import timezone
+
+
+class DistributionTestCase(TestCase):
+     def setUp(self) -> None:
+          Distribution.objects.create(
+             start_datetime=...
+          )
+
+     
 
 
 
